3be49a02f3180aa9887a3dc6046e3fc6caefbb3f
dardonosti <dardonostiok@gmail.com>
2020-01-16 19:34:53 -0300

    login & backendç

--- gpro-web/ClientApp/src/components/HomePage.js
+++ gpro-web/ClientApp/src/components/HomePage.js
@@ -20,13 +20,13 @@ export class HomePage extends Component {
         const { currentUser, users } = this.state;
         return (
             <div>
-                <h1>Hi {currentUser.firstName}!</h1>
+                <h1>Hi {currentUser.username}!</h1>
                 <p>You're logged in with React & JWT!!</p>
                 <h3>Users from secure api end point:</h3>
                 {users &&
                     <ul>
-                        {users.map(user =>
-                            <li key={user.id}>{user.firstName} {user.lastName}</li>
+                    {users.map(user =>
+                        <li key={user.id}>{user.username} </li>
                         )}
                     </ul>
                 }
@@ -34,5 +34,3 @@ export class HomePage extends Component {
         );
     }
 }
-
-export default HomePage;
--- gpro-web/ClientApp/src/index.js
+++ gpro-web/ClientApp/src/index.js
@@ -5,13 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { configureFakeBackend } from './components/fake-backend';
+//import { configureFakeBackend } from './components/fake-backend';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 
-configureFakeBackend();
+//configureFakeBackend();
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
