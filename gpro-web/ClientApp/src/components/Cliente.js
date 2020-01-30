2bf4f65a0043c41193775d0878d5c23f0ffd1731
dardonosti <dardonostiok@gmail.com>
2020-01-29 23:27:42 -0300

    modificaciones varias: mostrar resultado, limpieza de código, y botón limpiar

--- gpro-web/ClientApp/src/components/Cliente.js
+++ gpro-web/ClientApp/src/components/Cliente.js
@@ -15,7 +15,8 @@ export class Cliente extends Component {
         this.state = {
             currentUser: authenticationService.currentUserValue,
             users: null,
-            consulta: []
+            consulta: [],
+            mostrar: false
         };
     }
 
@@ -26,8 +27,14 @@ export class Cliente extends Component {
         }
     }
 
+    limpiarPantalla = () => {
+        this.setState({
+            mostrar: false
+        });
+    }
+
     render() {
-        const { consulta } = this.state;
+        const { consulta, mostrar } = this.state;
         return (
             <div>
                 <Formik
@@ -57,7 +64,8 @@ export class Cliente extends Component {
                             clienteService.getByString(values.dato)
                                 .then(
                                     consulta => {
-                                        this.setState({ consulta })
+                                        this.setState({ consulta });
+                                        this.setState({ mostrar: true });
                                         setSubmitting(false);
                                     },
                                     error => {
@@ -70,7 +78,8 @@ export class Cliente extends Component {
                             clienteService.getById(values.cuit)
                                 .then(
                                     consulta => {
-                                        this.setState({ consulta: [consulta] })
+                                        this.setState({ consulta: [consulta] });
+                                        this.setState({ mostrar: true });
                                         setSubmitting(false);
                                     },
                                     error => {
@@ -81,63 +90,60 @@ export class Cliente extends Component {
                     }
                     }
                 >
-                    {({ map, errors, status, touched, isSubmitting }) => (
-
-                        <div className="container-fluid minh-100">
-                            <div className="container">
-                                <div className="row justify-content-flex-start align-items-flex-start minh-100">
-                                    <div className="buscar-clientes">
-                                        <div className="card border-0">
-                                            <div className="card-body">
-                                                <Form>
-                                                    <div className="form-group txt-color">
-                                                        Nombre/Apellido/Razón social:
+                    {({ errors, status, touched, isSubmitting }) => (
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <Form>
+                                        <div className="form-group txt-color">
+                                            Nombre/Apellido/Razón social:
                                                         <Field name="dato" type="text" className={'form-control' + (errors.dato && touched.dato ? ' is-invalid' : '')} />
-                                                        <ErrorMessage name="dato" component="div" className="invalid-feedback" />
-                                                        CUIT:
+                                            <ErrorMessage name="dato" component="div" className="invalid-feedback" />
+                                            CUIT:
                                                         <Field name="cuit" type="text" className={'form-control' + (errors.cuit && touched.cuit ? ' is-invalid' : '')} />
-                                                        <ErrorMessage name="cuit" component="div" className="invalid-feedback" />
-                                                    </div>
-
-                                                    <div className="form-group">
-                                                        <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Buscar</button>
-                                                    </div>
-                                                    {status &&
-                                                        <div className={'alert alert-danger'}>Búsqueda sin resultados.</div>
-                                                    }
-                                                </Form>
-                                                <div className="container">
-                                                    <thead>
-                                                        <tr>
-                                                            <th className="column-tb1">CUIT</th>
-                                                            <th className="column-tb3">Razon Social</th>
-                                                            <th className="column-tb2">Apellido</th>
-                                                            <th className="column-tb2">Nombre</th>
-                                                            <th className="column-tb3">Domicilio</th>
-                                                            <th className="column-tb1">Teléfono</th>
-                                                            <th className="column-tb2">E-Mail</th>
-
-                                                        </tr>
-                                                    </thead>
-                                                    {consulta.map(cons =>
-                                                        <tr>
-                                                            <td className="column-tb1">{cons.idCliente}</td>
-                                                            <td className="column-tb3">{cons.razonSocialCliente}</td>
-                                                            <td className="column-tb2">{cons.apellidoCliente}</td>
-                                                            <td className="column-tb2">{cons.nombreCliente}</td>
-                                                            <td className="column-tb3">{cons.direccionCliente}</td>
-                                                            <td className="column-tb1">{cons.telefonoCliente}</td>
-                                                            <td className="column-tb2">{cons.emailCliente}</td>
-                                                        </tr>)}
-                                                </div>
-                                            </div>
+                                            <ErrorMessage name="cuit" component="div" className="invalid-feedback" />
                                         </div>
-                                    </div>
+
+                                        <div className="form-group">
+                                            <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Buscar</button>
+                                            <button type="reset" className="btn btn-primary ml-4" onClick={this.limpiarPantalla} disabled={isSubmitting}>Limpiar</button>
+                                        </div>
+                                        {status &&
+                                            <div className={'alert alert-danger'}>Búsqueda sin resultados.</div>
+                                        }
+                                    </Form>
+
                                 </div>
                             </div>
                         </div>
                     )}
                 </Formik>
+                {mostrar &&
+                    <div>
+                        <div className="container">
+                            <thead>
+                                <tr>
+                                    <th className="column-tb1">CUIT</th>
+                                    <th className="column-tb3">Razon Social</th>
+                                    <th className="column-tb2">Apellido</th>
+                                    <th className="column-tb2">Nombre</th>
+                                    <th className="column-tb3">Domicilio</th>
+                                    <th className="column-tb1">Teléfono</th>
+                                    <th className="column-tb2">E-Mail</th>
+                                </tr>
+                            </thead>
+                            {consulta.map(cons =>
+                                <tr>
+                                    <td className="column-tb1">{cons.idCliente}</td>
+                                    <td className="column-tb3">{cons.razonSocialCliente}</td>
+                                    <td className="column-tb2">{cons.apellidoCliente}</td>
+                                    <td className="column-tb2">{cons.nombreCliente}</td>
+                                    <td className="column-tb3">{cons.direccionCliente}</td>
+                                    <td className="column-tb1">{cons.telefonoCliente}</td>
+                                    <td className="column-tb2">{cons.emailCliente}</td>
+                                </tr>)}
+                        </div>
+                    </div>}
             </div>
         );
     }
